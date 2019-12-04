b682550079a602794a294ad61db4bdc540abe91d
Tizmo1928 <tizmo1928@gmail.com>
2019-12-05 00:54:38 +0100

    nearly done3

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const JwtStrat = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const keys = require("../config/Keys");
+const keys = require("../config/Keys").secret;
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
